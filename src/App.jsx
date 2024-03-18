ad2c2bdd8c9dcb2eb95e06b285afe0ea3ffe6a88
Prithivi36 <prithivipappian@gmail.com>
2024-03-18 21:25:39 +0530

    Gh-Pages test

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="ClassroomManagement">
           <Navbar />
         <Routes>
             <Route path='/:role/:id' element={<Home />} />
