ad4a3e1a6efad5ff968e3992320060bd136040d1
Prithivi36 <prithivipappian@gmail.com>
2024-03-19 18:25:50 +0530

    enabeled cloud backend AWS

--- src/Api/StudentApi.js
+++ src/Api/StudentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Baseurl="http://192.168.1.2:8080/"
+const Baseurl="http://43.205.8.40:8080/"
 
 export function getAllStudent(){
     return axios.get(Baseurl+'student/getAll')
