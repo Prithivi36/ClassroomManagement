4d74260901efe743864daf622833791740d0ea16
Prithivi36 <prithivipappian@gmail.com>
2024-03-20 20:04:48 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename="/ClassroomManagement" >
+      <BrowserRouter >
           <Navbar />
         <Routes>
             <Route path='/' element={<Login />} />
