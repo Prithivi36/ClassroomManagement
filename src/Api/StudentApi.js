904f07de8918e672d1309b2f8560afe28b36c91d
Prithivi <prithivipappian@gmail.com>
2024-04-12 10:42:53 +0530

    Update StudentApi.js

--- src/Api/StudentApi.js
+++ src/Api/StudentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Baseurl="https://www.vcr.software:8080/"
+export const Baseurl="https://www.vcr.software:8080/"
 
 
 axios.interceptors.request.use(function(config){
